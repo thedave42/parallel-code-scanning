1baba156f3975ad422f3f7fc97715bb84f576da6
Dave Bloom <dave@digitalnoise.net>
2022-08-18 10:11:58 -0700

    Testing dynamic PR scan

--- project-2/scripts/babel/transform-prevent-infinite-loops.js
+++ project-2/scripts/babel/transform-prevent-infinite-loops.js
@@ -3,7 +3,7 @@
  * Copyright (c) 2017, Amjad Masad
  *
  * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * LICENSE file in the root directory of this source tree. Sonm edit
  */
 
 'use strict';
--- python-project/list-changed-langs.py
+++ python-project/list-changed-langs.py
@@ -4,7 +4,9 @@
 #
 # ["foo", "bar", "baz"]
 #
+from genericpath import isdir
 import json
+import os
 
 javascript = [".js", ".jsx", ".mjs", ".es", ".es6", ".htm", ".html", ".xhtm", ".xhtml", ".vue", ".hbs", ".ejs", ".njk", ".json", ".yaml", ".yml", ".raml", ".xml"]
 typescript = [".ts", ".tsx", ".mts", ".cts"]
@@ -15,9 +17,17 @@ python_lang = [".py"]
 java = [".java"]
 ruby = [".rb", ".erb", ".gemspec", "Gemfile"]
 
-
 lines = list(open("./.github/scripts/diff.txt").readlines())
-outlines = set()
+outlines = dict()
+outlines["include"] = set()
+
+def serialize_sets(obj):
+    if isinstance(obj, set):
+        l = list()
+        for item in obj:
+            if isinstance(item, tuple):
+                l.append(dict((x, y) for x, y in item))
+        return l
 
 def find_in_list(list, string):
     for item in list:
@@ -27,13 +37,22 @@ def find_in_list(list, string):
 
 #only add items that are directories
 for line in lines:
-    if find_in_list(javascript, line):
-        outlines.add("javascript")
-    if find_in_list(typescript, line):
-        outlines.add("javascript")
-    if find_in_list(javascript, line):
-        outlines.add("javascript")
-
-
-
-print(json.dumps(list(outlines)))
+    path = line.split('/')[0]
+    if find_in_list(javascript, line) and (os.path.isdir(path)):
+        outlines["include"].add(tuple(dict({"target-dir": path, "languages": "javascript"}).items()))
+    if find_in_list(typescript, line) and (os.path.isdir(path)):
+        outlines["include"].add(tuple(dict({"target-dir": path, "languages": "javascript"}).items()))
+    if find_in_list(c_and_cplus, line) and (os.path.isdir(path)):
+        outlines["include"].add(tuple(dict({"target-dir": path, "languages": "cpp"}).items()))
+    if find_in_list(csharp, line) and (os.path.isdir(path)):
+        outlines["include"].add(tuple(dict({"target-dir": path, "languages": "csharp"}).items()))
+    if find_in_list(golang, line) and (os.path.isdir(path)):
+        outlines["include"].add(tuple(dict({"target-dir": path, "languages": "go"}).items()))
+    if find_in_list(python_lang, line) and (os.path.isdir(path)):
+        outlines["include"].add(tuple(dict({"target-dir": path, "languages": "python"}).items()))
+    if find_in_list(java, line) and (os.path.isdir(path)):
+        outlines["include"].add(tuple(dict({"target-dir": path, "languages": "java"}).items()))
+    if find_in_list(ruby, line) and (os.path.isdir(path)):
+        outlines["include"].add(tuple(dict({"target-dir": path, "languages": "ruby"}).items()))
+    
+print(json.dumps(outlines, default=serialize_sets))
